72d52c0b56b8ee26b040eb7605a44883a5717096
Yuvi <81169447+YUVRAJg37@users.noreply.github.com>
2021-11-27 17:46:15 +0530

    Added Extraction Zone

---
+++ Source/FPSGame/Private/FPSExtractionZone.cpp
@@ -0,0 +1,47 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "FPSExtractionZone.h"
+#include "Components/DecalComponent.h"
+
+// Sets default values
+AFPSExtractionZone::AFPSExtractionZone()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	OverlapComp = CreateDefaultSubobject<UBoxComponent>("Overlap Component");
+	OverlapComp->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	OverlapComp->SetCollisionResponseToChannels(ECR_Ignore);
+	OverlapComp->SetCollisionResponseToChannel(ECC_Pawn, ECR_Overlap);
+	OverlapComp->bHiddenInGame = false;
+	OverlapComp->SetBoxExtent(FVector(200.0f));
+	RootComponent = OverlapComp;
+
+	DecalComponent = CreateDefaultSubobject<UDecalComponent>("Decal Component");
+	DecalComponent->SetupAttachment(GetRootComponent());
+	DecalComponent->DecalSize = FVector(200.0f);
+}
+
+// Called when the game starts or when spawned
+void AFPSExtractionZone::BeginPlay()
+{
+	Super::BeginPlay();
+
+	OverlapComp->OnComponentBeginOverlap.AddDynamic(this, &AFPSExtractionZone::OnComponentBeginOverlap);
+	
+}
+
+// Called every frame
+void AFPSExtractionZone::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void AFPSExtractionZone::OnComponentBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	UE_LOG(LogTemp, Warning, TEXT("Overlapping"));
+}
+
---
+++ Source/FPSGame/Public/FPSExtractionZone.h
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/BoxComponent.h"
+#include "GameFramework/Actor.h"
+#include "FPSExtractionZone.generated.h"
+
+UCLASS()
+class FPSGAME_API AFPSExtractionZone : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AFPSExtractionZone();
+
+	UFUNCTION()
+	void OnComponentBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult);
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditDefaultsOnly, Category = Components)
+	UBoxComponent* OverlapComp;
+	UPROPERTY(EditDefaultsOnly, Category = Components)
+	UDecalComponent* DecalComponent;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
